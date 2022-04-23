1ae57b6dfefd53b661d2f51cb015cefb73571892
Tommi <99382026+tommi-lew-tw@users.noreply.github.com>
2022-04-23 09:55:29 +0800

    remove Lombok from BasketItem
    Can’t seem to get it to generate the getters and setters. Try again later.

--- src/main/java/BasketItem.java
+++ src/main/java/BasketItem.java
@@ -1,21 +1,32 @@
-import lombok.Getter;
-import lombok.Setter;
-
 /*
- * Represents a Item added into a Basket.
+ * Represents an Item added into a Basket.
  */
 public class BasketItem {
     private final Item item;
 
     public int price;
 
-    @Getter
-    @Setter
     private int quantity;
 
     public BasketItem(Item item, int price, int quantity) {
         this.item = item;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
